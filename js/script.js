f93650983af0d2da719925323375f3e1d04dd2f3
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-08-02 22:44:00 +0300

    les_04 OOP func

--- js/script.js
+++ js/script.js
@@ -12,160 +12,173 @@
 // }
 
 
-
-let numberOfFilms;
-
 let personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     private: false,
-};
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
-}
-
-start();
-
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt("Один из последних просмотренных фильмов?", "");
-        const b = +prompt("На сколько оцените его?", "");
-        if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log("DONE.")
-        } else {
-            console.log("ERROR");
-            i--;
+    start: () => {
+        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+        while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
         }
+    },
+    rememberMyFilms: () => {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt("Один из последних просмотренных фильмов?", "");
+            const b = +prompt("На сколько оцените его?", "");
+            if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log("DONE.")
+            } else {
+                console.log("ERROR");
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: () => {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        }
+        if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        }
+        if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман")
+        } else {
+            console.log("Произошла ошибка")
+        }
+    },
+    showMyDB: (hidden) => {
+        if (!hidden) {
+            console.log(personalMovieDB)
+        }
+    },
+    toggleVisibleMyDB: () => {
+        personalMovieDB.private = !personalMovieDB.private;
+    },
+    writeYourGenres: () => {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+            if (genre === "" || genre === null) {
+                console.log(" Не корректные данные");
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+
+            personalMovieDB.genres[i - 1] = genre;
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        })
     }
 }
 
-rememberMyFilms();
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов");
-    }
-    if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Вы классический зритель");
-    }
-    if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман")
-    } else {
-        console.log("Произошла ошибка")
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB)
-    }
-}
-showMyDB(personalMovieDB.private);
-
-
-function writeYourGenres() {
-    for(let i = 1; i <= 3; i++){
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-
-writeYourGenres();
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// let num = 20;
-//
-// showFirstMessage = (text) => {
-//     console.log(text);
-//     console.log(num);
-// }
-//
-// showFirstMessage("Приветик жопка");
-// console.log(num);
-//
-// calc = (a, b) => {
-//     return a + b;
-// }
-//
-// console.log(calc(4, 6));
-// console.log(calc(2, 7));
-// console.log(calc(1, 43));
-//
-//
-//
-// const ret = () => {
-//     let num = 50;
-//     return num;
+// function first() {
+//     setTimeout(function () {
+//         console.log(1)
+//     }, 500);
 // }
 //
 //
-// const anotherNum = ret();
-// console.log(anotherNum);
-
-// const str = "teSt";
+// function second() {
+//     console.log(2)
+// }
 //
-// // console.log(str.toUpperCase());
-// console.log(str.toLowerCase());
+// first();
+// second();
 //
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
 //
-// console.log(str);
-// // console.log(arr.length)
+// function done() {
+//     console.log("Я прошёл этот урок")
+// }
 //
-// let fruit = "Some fruit";
+// learnJS("JavaScript", done);
+
+
+// const options = {
+//     name: "test",
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: "black",
+//         bg: "red"
+//     }
+// };
+
+// console.log(options.name);
 //
-// console.log(fruit.indexOf("fruit"));
+// delete options.name;
 //
+// console.log(options);
+
+// for (let key in options){
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`)
+// }
+
+// const arr = [1,2,3,4,5,6,7,32];
 //
-// const logg = "Hello World";
+// arr.forEach( (item, i, arr) => {
 //
-// console.log(logg.slice(6, 11)) // ВЫРЕЗАЕТ ФРАГМЕНТ
+//     console.log(`${i}: ${item} внутри массива ${arr}`)
+// } )
+
+// const obg = {
+//     a: 5,
+//     b: 1,
+//     c: {
+//         a: 421,
+//         b: 65234
+//     }
+// };
 //
+// const copy = {
+//     ...obg,
+//     a: 321,
+//     b: 432,
+//     c: {
+//         ...obg.c,
+//         a: "694234236234",
+//         b: "sdifhdupig"
+//     }
 //
-// console.log(logg.substring(6, 11))
+// }
 //
+// console.log(obg)
+// console.log(copy)
+
+
+// const numbers = {
+//     a: 2,
+//     b: 6,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
 //
-// console.log(logg.substr(6, 5)) // ПЕРВОЕ ЗНАЧЕНИЕ = СТАРТ, ВТОРОЕ СКОЛЬКО СИМОВЛОВ ОТРЕЗАТЬ СПРАВА
+// const newNumbers = copy(numbers);
 //
-// const num = 12.2;
-// console.log(Math.round(num))
+// newNumbers.a = 10;
 //
-// const test = "12.2px"
+// let log = (a,b,c) => {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
 //
-// console.log(parseInt(test))
-// console.log(parseFloat(test))
+// const num  = [2, 5, 7];
+//
+// log(...num);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const a = [5432,5, 542,]
+//
+// console.log(a);
